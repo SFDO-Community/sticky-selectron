5a6c1f15d39a6cdb98f5e28703d203dc79dea819
David Pickett <pickettd@gmail.com>
2024-11-21 16:27:57 -1000

    Add logic in the Apex side to return the type of each field and the scale for Doubles. Add logic in the main and data-cell LWCs to receive the type/scale info and correctly display

--- force-app/main/default/lwc/stickySelectronMain/stickySelectronMain.js
+++ force-app/main/default/lwc/stickySelectronMain/stickySelectronMain.js
@@ -100,12 +100,18 @@ export default class StickySelectronMain extends LightningElement {
                     this.inputTableFieldNames.length
                 ) {
                     this.inputTableFieldNames.forEach((fieldName) => {
-                        const fieldLabel = fieldInfo.fieldLabels[fieldName];
+                        const fieldLabel =
+                            fieldInfo.fieldLabels[fieldName].label;
+                        const fieldType = fieldInfo.fieldLabels[fieldName].type;
+                        const fieldScale =
+                            fieldInfo.fieldLabels[fieldName].scale;
                         if (fieldLabel) {
                             this.fieldsOnLeft.push({
                                 label: fieldLabel,
                                 fieldname: fieldName,
-                                isDynamic: true
+                                isDynamic: true,
+                                sfType: fieldType,
+                                sfScale: fieldScale
                             });
                         } else {
                             this.showError(
@@ -126,7 +132,8 @@ export default class StickySelectronMain extends LightningElement {
                             this.fieldsOnRight.push({
                                 label: fieldLabel,
                                 fieldname: fieldName,
-                                isDynamic: true
+                                isDynamic: true,
+                                sfType: ''
                             });
                         } else {
                             this.showError(
